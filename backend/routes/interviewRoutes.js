2ea619e2a3be49b66088763cca051bd702861428
ritiksingh <ritikkumarsinghdeo6@gmail.com>
2026-05-26 12:16:23 +0530

    Initial commit

---
+++ backend/routes/interviewRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { handleInterview } from "../controllers/interviewController.js";
+
+const router = express.Router();
+
+
+router.post("/", handleInterview);
+
+export default router;
